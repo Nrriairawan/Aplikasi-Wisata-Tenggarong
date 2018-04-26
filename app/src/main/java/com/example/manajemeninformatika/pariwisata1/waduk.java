42a8e5ef42ec066b9379ae888af7d7a8f2edd0d0
lilisnawati <nawatililisnawati@gmail.com>
2018-04-26 16:19:53 +0800

    menambahkan layout

---
+++ app/src/main/java/com/example/manajemeninformatika/pariwisata1/waduk.java
@@ -0,0 +1,13 @@
+package com.example.manajemeninformatika.pariwisata1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class waduk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waduk);
+    }
+}
