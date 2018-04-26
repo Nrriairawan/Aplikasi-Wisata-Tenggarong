db678e23ad498995018088577d20525952119926
lilisnawati <nawatililisnawati@gmail.com>
2018-04-26 13:13:53 +0800

    buat layout

---
+++ app/src/main/java/com/example/manajemeninformatika/pariwisata1/kategori.java
@@ -0,0 +1,13 @@
+package com.example.manajemeninformatika.pariwisata1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kategori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategori);
+    }
+}
