edc853bbb31f55408f6a4b32073b91e5b0d27550
Nrriairawan <nrriairawan2330@gmail.com>
2018-05-10 19:40:31 +0800

    udh nda error di layout kategori

--- app/src/main/java/com/example/manajemeninformatika/pariwisata1/kategori.java
+++ app/src/main/java/com/example/manajemeninformatika/pariwisata1/kategori.java
@@ -17,9 +17,6 @@ public class kategori extends AppCompatActivity {
     @BindView(R.id.btn_sejarah) EditText btn_sejarah;
     @BindView(R.id.btn_peta) EditText btn_peta;
     @BindView(R.id.btn_wisata) EditText btn_wisata;
-    @BindView(R.id.btn_sejarah) Button btn_sejarah;
-    @BindView(R.id.btn_wisata) Button btn_wisata;
-    @BindView(R.id.btn_peta) Button btn_peta;
     @BindView(R.id.imageViewmulawarman) ImageView imageViewmulawarman;
     @BindView(R.id.imageViewplanetarium) ImageView imageViewplanetarium;
     @BindView(R.id.imageViewpulaukumala) ImageView imageViewpulaukumala;
